7da3284a919c3ddaf20782cb8f7b63fbb18249e5
BishowjitRoy <bishowjitroy01@gmail.com>
2022-01-16 11:50:32 -0600

    message

--- src/main/java/variousConcepts/GitTest1.java
+++ src/main/java/variousConcepts/GitTest1.java
@@ -5,7 +5,7 @@ public class GitTest1 {
 	public static void main(String[] args) {
 		System.out.println("This is for Git Test !!!");
 		System.out.println("This is for Git Test1 !!!");
-		System.out.println("This is for Git Test2 !!!");
+		System.out.println("Modification done for Git Test2 !!!");
 		System.out.println("This is for Git Test3 !!!");
 
 	}
